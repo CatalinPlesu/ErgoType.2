eee7b3b24c156a74142a124d5e0057e3e3aec0cc
Catalin Plesu <catalinhimself@gmail.com>
2025-10-08 18:50:16 +0300

    basic fitness

--- src/core/typer.py
+++ src/core/typer.py
@@ -9,6 +9,10 @@ Extensibility: Can incorporate additional cost factors (e.g., finger effort, alt
 
 from src.config.config import Config
 from src.core.mapper import KeyType
+from src.core.keyboard import FingerName, enums_to_fingername
+
+HOMING_POSITION = 'homing'
+CURRENT_POSITION = 'current'
 
 
 class Typer:
@@ -19,7 +23,26 @@ class Typer:
         self.distance = distance
         self.layout = layout
         self.dataset = dataset
+        self.load_finger_positions()
         self.fitness(words=False)
+
+    def load_finger_positions(self):
+        self.finger = {finger:
+                       {HOMING_POSITION: self.keyboard.get_homing_key_for_finger_name(
+                           finger).id} for finger in FingerName}
+        self.reset_finger_position()
+
+    def reset_finger_position(self):
+        for finger in self.finger:
+            self.finger[finger][CURRENT_POSITION] = self.finger[finger][HOMING_POSITION]
+        self.moved_fingers = []
+
+    def move_finger(self, finger, key_to):
+        self.moved_fingers.append(key_to)
+        key_from = self.finger[finger][CURRENT_POSITION]
+        self.finger[finger][CURRENT_POSITION] = key_to
+        d, m = self.distance.get_distance_and_movement(key_from, key_to)
+        return d
 
     def fitness(self, words=True, symbols=True, fluid_typing=False):
         match (words, symbols, fluid_typing):
@@ -47,6 +70,7 @@ class Typer:
             lambda key_id, layer_id, value: value.key_type == KeyType.CONTROL and value.value == 'Shift')
 
         shift_key = None
+        shift_key_id = None
         print(f"shift keys {shift_keys}")
 
         for key, value in self.dataset[Config.dataset.field_character_frequencies].items():
@@ -55,8 +79,9 @@ class Typer:
             total_percentage += relative
             # data = self.layout.mapper.filter_data(
             #     lambda key_id, layer_id, value:  key in value)
+            print(f"seraching for charater '{key}'")
             key_id, layer, qmk_key = self.layout.find_key_for_char(key)
-            print(key_id, layer)
+            # print(key_id, layer)
             if key in shifted:
                 for shift in shift_keys:
                     shift_id, _ = shift
@@ -65,10 +90,25 @@ class Typer:
                         break
             print(f""" key {key} - key_id {key_id} """)
             if shift_key is not None:
-                print(f""" shift - key_id {shift_key[0][0]} """)
+                shift_key_id = shift_key[0][0]
+                print(f""" shift - key_id {shift_key_id} """)
+
+                fingername = enums_to_fingername(
+                    self.keyboard.keys[shift_key_id].finger, self.keyboard.keys[shift_key_id].hand)
+                if isinstance(fingername, list):
+                    fingername = fingername[0]
+                score += self.move_finger(fingername, shift_key_id)
+
+            fingername = enums_to_fingername(
+                self.keyboard.keys[key_id].finger, self.keyboard.keys[key_id].hand)
+            if isinstance(fingername, list):
+                fingername = fingername[0]
+            score += self.move_finger(fingername, key_id)
+            self.reset_finger_position()
 
         print(f"""Total percentage of key pressed is '{
               total_percentage}', it should be almost 100""")
+        print(f"""Total score {score}""")
 
     def _print(self, *args, **kwargs):
         if self.debug:
